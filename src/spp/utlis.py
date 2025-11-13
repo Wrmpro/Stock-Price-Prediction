4fbc3b365f1903e07c6a6e5d2bfb75d82856550b
Shashank-07 <96239397+Wrmpro@users.noreply.github.com>
2025-11-13 09:35:57 +0530

    Update utlis.py

--- src/spp/utlis.py
+++ src/spp/utlis.py
@@ -1,1 +1,8 @@
 
+def evaluate_model(y_true, y_pred):
+    from sklearn.metrics import mean_squared_error
+    import numpy as np
+    mse = mean_squared_error(y_true, y_pred)
+    rmse = np.sqrt(mse)
+    return {"MSE": mse, "RMSE": rmse}
+
